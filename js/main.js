e7496a6a311df15f1b77e1ca111b6d153ae27e0e
Jisu <jaejisu@gmail.com>
2017-01-28 00:32:00 -0800

    add gitignore file and add category section in .js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,42 @@
 
 $(function(){
-
 	$('.section').on('change', function(){
-		var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-		url += '?' + $.param({
-		'api-key': "6689b79959214fb6b3222a74112dbce3"
-		});
-		
+		$('.menu').toggleClass('menu-up').css('height', 'auto');
+		// $().toggleClass('logoscale')
+
+			$('.list').empty();
+			var catagory = $(this).val();
+
+			var url = 'https://api.nytimes.com/svc/topstories/v2/' + catagory + '.json';
+			url += '?' + $.param({
+			'api-key': '6689b79959214fb6b3222a74112dbce3'});
 		
 		$.getJSON(url)
+
+	
 		.done(function(data){
-			console.log('hello world');
+		var results = data.results.filter(function(item){
+				return item.multimedia.length;
+		}).splice(0,12);
 
-			// for (var i = 0; i < 12; i++) {
-			// 	console.log(data.results[i].multimedia[0].url);
-				// if 
-				// else//(if there is no photo skip it)
-			// }
 
-			$.each(data.results,function(key, value){
-				$('.content').append('<p>'+value.title+'</p><img src ="'+value.multimedia[3].url+'">');
-				console.log(value);
+		var $finalimage = $('.list')
+		var show = '';
+
+		$.each(results, function(data, value){
+			console.log(value)
+			var main = value.abstract;
+			var img = value.multimedia[4].url;
+
+			show += '<li class="li-article"><div id="wrapper"><p class="text">'
+			show += main
+			show += '</p></div>'
+			show += '<img class ="li-image" src="' 
+			show += img
+			show += '"/></li>'
 		})
-		
+		$finalimage.append(show);
+
 		})
-
-
-
-
 	})
 });
-
-
