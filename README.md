21cc1a7fb1a6b822e85e403f6605d95eb698b57e
Jisu <jaejisu@gmail.com>
2017-01-28 23:12:39 -0800

    find the way of layiut the images without using flexbox backgrounrund  background

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# instanews_app_project02
+# Instanews App (project02)
 create a responsive website that allows a user to filter top news story 
 categories via the New York Times API.
 
--- js/main.js
+++ js/main.js
@@ -1,45 +1,50 @@
 
-$(function(){
+$(function() {
 	$('.loading').hide();
-	$('.section').on('change', function(){
-		$('.menu').toggleClass('menu-up').css('height', 'auto');
+	$('.section').on('change', function() {
+		$('.menu').toggleClass('menu-up').css('max-height', '200');
 		$('.logo').toggleClass('logoscale');
+		// $('.menu').toggleClass('menu-height');
 			
-			$('.loading').show();
-			$('.list').empty();
-			var catagory = $(this).val();
+		// $('.loading').show();
+		$('.list').empty();
+		var catagory = $(this).val();
 
-			var url = 'https://api.nytimes.com/svc/topstories/v2/' + catagory + '.json';
-			url += '?' + $.param({
-			'api-key': '6689b79959214fb6b3222a74112dbce3'});
+		var url = 'https://api.nytimes.com/svc/topstories/v2/' + catagory + '.json';
+		url += '?' + $.param({'api-key': '6689b79959214fb6b3222a74112dbce3'});
 		
-		$.getJSON(url)
-
-	
-		.done(function(data){
+		$.getJSON(url).done(function(data){
 			$('.loading').hide();
-		var results = data.results.filter(function(item){
+			var results = data.results.filter(function(item){
 				return item.multimedia.length;
-		}).splice(0,12);
+			}).splice(0,12);
 
+			var $finalimage = $('.list');
+			var show = '';
 
-		var $finalimage = $('.list')
-		var show = '';
+			$.each(results, function(data, value){
+				console.log(value);
+				var main = value.abstract;
+				var img = value.multimedia[4].url;
 
-		$.each(results, function(data, value){
-			console.log(value)
-			var main = value.abstract;
-			var img = value.multimedia[4].url;
-
-			show += '<li class="li-article"><div id="wrapper"><p class="text">'
-			show += main
-			show += '</p><div class="text-bg"></div></div>'
-			show += '<img class ="li-image" src="' 
-			show += img
-			show += '"/></li>'
-		})
-		$finalimage.append(show);
-
-		}).fail()
-	})
+				show += '<li class="li-article">';
+				show += 	'<div class="article-wrapper">';
+				show += 		'<div class="text-wrapper">';
+				show += 			'<p>';
+				show += 				main;
+				show += 			'</p>';
+				show += 		'</div>';
+				show +=			'<img class ="li-image" src="';
+				show += 			img;
+				show += 		'"alt="image"/>';
+				show +=		'</div>';
+				show += '</li>';
+			});
+			$finalimage.append(show);
+		}).fail(function() {
+			$('.message').append('Sorry there was a problem.');	
+		}).always(function() {
+			$('.loading').show();
+		});
+	});
 });
