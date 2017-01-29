ffc7efd527745f50e8073a1594c617432a501234
Jisu <jaejisu@gmail.com>
2017-01-29 15:18:24 -0800

    testing

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 
 $(function() {
-	$('.loading').hide();
+
 	$('.section').on('change', function() {
-		$('.menu').toggleClass('menu-up').css('max-height', '200');
-		$('.logo').toggleClass('logoscale');
-		// $('.menu').toggleClass('menu-height');
+		$('.menu').toggleClass('menu-up').css('height', 'auto');
+		$('.logo').toggleClass('logo-scale');
 			
-		// $('.loading').show();
+		$('.loading').show();
 		$('.list').empty();
 		var catagory = $(this).val();
 
@@ -14,37 +13,40 @@ $(function() {
 		url += '?' + $.param({'api-key': '6689b79959214fb6b3222a74112dbce3'});
 		
 		$.getJSON(url).done(function(data){
-			$('.loading').hide();
 			var results = data.results.filter(function(item){
 				return item.multimedia.length;
 			}).splice(0,12);
 
 			var $finalimage = $('.list');
-			var show = '';
+			var article = '';
 
 			$.each(results, function(data, value){
 				console.log(value);
 				var main = value.abstract;
 				var img = value.multimedia[4].url;
+				var link = value.url;
 
-				show += '<li class="li-article">';
-				show += 	'<div class="article-wrapper">';
-				show += 		'<div class="text-wrapper">';
-				show += 			'<p>';
-				show += 				main;
-				show += 			'</p>';
-				show += 		'</div>';
-				show +=			'<img class ="li-image" src="';
-				show += 			img;
-				show += 		'"alt="image"/>';
-				show +=		'</div>';
-				show += '</li>';
+				article += '<li class="li-article">';
+				article += 	'<a href =';
+				article += 		link
+				article += 	'></a><div></div>'
+				article += 		'<div class="article-wrapper">';
+				article += 			'<div class="text-wrapper">';
+				article += 				'<p>';
+				article += 					main;
+				article += 				'</p>';
+				article += 			'</div>';
+				article +=				'<img class ="li-image" src="';
+				article += 				img;
+				article += 			'"alt="image"/>';
+				article +=		'</div>';
+				article += '</li>';
 			});
-			$finalimage.append(show);
+			$finalimage.append(article);
 		}).fail(function() {
 			$('.message').append('Sorry there was a problem.');	
 		}).always(function() {
-			$('.loading').show();
+			$('.loading').hide();
 		});
 	});
 });
